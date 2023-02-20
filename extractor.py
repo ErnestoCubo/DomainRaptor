20680dadeecc3849e903349a1541633397461ad4
Estiwer <30570774+ErnestoCubo@users.noreply.github.com>
2023-02-20 17:11:32 +0100

    Added multiple new features
    - Schema Export Function -> need to ve developed but is implemented
    - Added multiples import for multiprocessor execution
    - Modified regex patterns to more efficent and correct ones

--- extractor.py
+++ extractor.py
@@ -1,5 +1,8 @@
 import re
-import os
+import whois
+import logging
+import threading
+import time
 
 def open_file(file_path: str):
     text_file = open(file_path  , 'r')
@@ -13,11 +16,32 @@ def find_patterns(regex_expr: str, src_string: str):
 
     return finds
 
+def validate_domain():
+    return
+
+def export(option_selected):
+    export_format =  input("In which format do you want to export data:\n 1) JSON\n 2) CSV\n 3) YML\n 4) Plain text file\n 5) EXIT\nSelect an option: ")
+    match export_format:
+        case '1':
+            print('JSON\n')
+        case '2':
+            print('CSV\n')
+        case '3':
+            print('YML\n')
+        case '4':
+            print('TXT\n')
+        case '5':
+            return 0
+        case _:
+            print("[ERROR]::Invalid option")
+            exit(1)
+    return
+
 def main():
-    user_input = input("Which regex you want to use:\n 1) IP\n 2) URLs\n 3) Search for URLs and file acceses\nSelect an option: ")
+    user_input = input("Which regex you want to use:\n 1) IP\n 2) Split for Domains\n 3) Search for URLs and file acceses\n 4) Export\nSelect an option: ")
     match user_input:
         case '1':
-            expr = r"(?:\d{1,3}\.){3}\d{1,3}"
+            expr = r"\b(?:(?:(?:2[0-5]{2}|1[0-9]{2}|[1-9][0-9]|[0-9])\.){3}(?:2[0-5]{2}|1[0-9]{2}|[1-9][0-9]|[1-9]))"
         case '2':
             expr = r"^(http|https)(\:\/{2})(w{3}\.)?([a-zA-Z0-9!@#$&()%-`.+,/\"]+)(\.[a-z]{1,5})"
         case '3':
@@ -29,9 +53,11 @@ def main():
     file_path = './patterns.txt'
     file_contents = open_file(file_path)
     print("\nPrinting file content:" + file_contents + "\n")
+    print(time.clock_gettime())
     finds = find_patterns(expr, file_contents)
     print("The matched patterns are: \n")
     print(finds)
+    export(user_input)
     return 0
 
 if __name__ == '__main__':
